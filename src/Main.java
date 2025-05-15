b50620632cbdff574dd7880277ccdd04eb7a693f
SHOHRUH <shohruhmukhamedov6@gmail.com>
2025-05-15 14:42:32 +0500

    skjdfowjoj

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+   // salom qalesan
+        ArrayList<Integer> arrayList=new ArrayList<>();
 
     }
 }
